f21e3230f83ddec058a01dfd22931726029441a9
projectallies <wardag.as@gmail.com>
2016-02-20 13:47:35 +0430

    add new model Pages

--- news/models.py
+++ news/models.py
@@ -2,4 +2,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class Pages(models.Model):
+    Title = models.CharField(max_length=200, verbose_name='Pages Title')
+
 # Create your models here.
