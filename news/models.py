837d7b210fc5a17d789cb3c9f49aa652c6a83d0d
projectallies <wardag.as@gmail.com>
2016-02-20 13:53:29 +0430

    add new model Pages
    register pages to admin site
    
    add self in model

--- news/models.py
+++ news/models.py
@@ -4,5 +4,7 @@ from django.db import models
 
 class Pages(models.Model):
     Title = models.CharField(max_length=200, verbose_name='Pages Title')
+    def __str__(self):
+        return self.Title
 
 # Create your models here.
