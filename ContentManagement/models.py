0b155a6e5bae0e4084048a9a56f98cbc2c32ed2e
projectallies <wardag.as@gmail.com>
2016-02-20 14:36:36 +0430

    Change pages to page

--- ContentManagement/models.py
+++ ContentManagement/models.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
-class Pages(models.Model):
+class Page(models.Model):
     Title = models.CharField(max_length=200, verbose_name="Page Title")
     # this is comment
     def __str__(self):
