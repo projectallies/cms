2912469bc5fde0cfa6c5e7027689a52192601ad2
projectallies <wardag.as@gmail.com>
2016-02-20 14:42:44 +0430

    Create Pages Model add media

--- cms/settings.py
+++ cms/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-MEDIA_ROOT = os.path.join(PROJECT_ROOT, '../media')
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, '../media') 
